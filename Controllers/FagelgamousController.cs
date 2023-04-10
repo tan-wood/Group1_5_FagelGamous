3c7f4a20ccf725e85ba7e283200ceb74ef1474ae
tannerwoodrum <tannerwoodrum@TANNERWOODR8C05>
2023-04-10 13:07:57 -0600

    Add project files.

---
+++ Controllers/FagelgamousController.cs
@@ -0,0 +1,16 @@
+﻿using Group1_5_FagelGamous.Data;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+
+namespace Group1_5_FagelGamous.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FagelgamousController : ControllerBase
+    {
+    }
+}
